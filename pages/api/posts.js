13f041129ad8400fbf8cc482a57126170a85dd0d
abhinavpurohit47 <purohitabhinav01@gmail.com>
2021-10-23 10:21:52 +0530

    addPost function

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,6 +2,50 @@ const { connectToDatabase } = require('../../lib/mongodb');
 
 const ObjectId = require('mongodb').ObjectId;
 
+async function getPosts(req,res){
+    try {
+        // connect to the database
+        let { db } = await connectToDatabase();
+        // fetch the posts
+        let posts = await db
+            .collection('posts')
+            .find({})
+            .sort({ published: -1 })
+            .toArray();
+        // return the posts
+        return res.json({
+            message: JSON.parse(JSON.stringify(posts)),
+            success: true,
+        });
+    } catch (error) {
+        // return the error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+}
+
+async function addPost(req, res) {
+    try {
+        // connect to the database
+        let { db } = await connectToDatabase();
+        // add the post
+        await db.collection('posts').insertOne(JSON.parse(req.body));
+        // return a message
+        return res.json({
+            message: 'Post added successfully',
+            success: true,
+        });
+    } catch (error) {
+        // return an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+}
+
 export default async function handler(req, res) {
     // switch the methods
     switch (req.method) {
@@ -22,4 +66,3 @@ export default async function handler(req, res) {
         }
     }
 }
-
